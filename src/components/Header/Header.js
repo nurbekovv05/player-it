3f04728ac9f082c85b95df8d89b12de8a03f0a14
Nurbolot <114340163+nurbekovv05@users.noreply.github.com>
2023-01-26 16:47:02 +0600

    header (#1)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,34 @@
 import React from 'react';
-
+import Logo from './../../image/логотип.png'
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div id="header">
+                <div className="container">
+                    <div className="header">
+                        <div className="header-left">
+                            <div className="header-left_logo">
+                                 <img src={Logo} alt=""/>
+                            </div>
+                            <div className="header-left_links">
+                                <a href='#' className="nav-header">О школе</a>
+                                <a href="#" className="nav-header" >Наши курсы</a>
+                                <a href="#" className="nav-header" >О нас</a>
+                            </div>
+                        </div>
+                        <div className="header-right">
+                            <div className="header-right_buttons">
+                                <a href="#" className="nav-right">Войти</a>
+                                <button className="button-header">Подписаться</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </>
     );
 };
 
